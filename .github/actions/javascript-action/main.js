a41dc89e1cb0a7711eedb2e6e6279c1ccb4f6ff0
Debanjan752 <debanjanhore752@gmail.com>
2025-05-18 23:06:53 +0530

    custom action added9

--- .github/actions/javascript-action/main.js
+++ .github/actions/javascript-action/main.js
@@ -1,9 +1,18 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 
 function run() {
-    core.notice('Hello From my custom JS action!')
+    // 1. get some input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketregion = core.getInput('bucket-region', {required: true});
+    const distfolder = core.getInput('dist-folder', {required: true});
+
+    // 2. upload files 
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distfolder} ${s3Uri} --region ${bucketregion}`);
+
+    core.notice('Hello From my custom JS action!');
 }
 run();
